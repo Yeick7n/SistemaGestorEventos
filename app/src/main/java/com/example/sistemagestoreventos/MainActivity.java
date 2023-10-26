4e13bf7c6a2c226bfbf3d1a8e7c4e1c4abae556b
Harold D <haroldalarcon613@gmail.com>
2023-10-26 13:18:20 -0500

    navigator view

--- app/src/main/java/com/example/sistemagestoreventos/MainActivity.java
+++ app/src/main/java/com/example/sistemagestoreventos/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.sistemagestoreventos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btninicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inicio);
+        btninicio = (Button) findViewById(R.id.btninicio);
+        btninicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
